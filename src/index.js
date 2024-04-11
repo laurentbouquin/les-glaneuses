c5ea75e411ce9a54608c84d904ae37af00fb5b7a
laurentbouquin <laurent.bouquin@algosup.com>
2024-04-11 12:23:32 +0200

    Add template

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let Home = document.getElementById('ButHome');
+Home.addEventListener('click', function() {
+  document.location.href = 'index.html';
+});
+
+let products = document.getElementById('ButProduct');
+products.addEventListener('click', function() {
+  document.location.href = 'products.html';
+});
+
+let About = document.getElementById('ButAbout');
+About.addEventListener('click', function() {
+  document.location.href = 'about.html';
+});
+
+let Login = document.getElementById('ButLog');
+Login.addEventListener('click', function() {
+  document.location.href = 'login.html';
+});
